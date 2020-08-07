a19e0b7e65bcb407e7e67a9f6b873a6e4e92a71f
Maksymilian Graczyk <maksymilian.graczyk18@imperial.ac.uk>
2020-08-07 17:51:18 +0200

    Add ReLU and Dummy activation classes

--- activation.py
+++ activation.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from sympy import Max, sign
+from devito import Eq
 
 
 class Activation(ABC):
@@ -9,5 +11,25 @@ class Activation(ABC):
         return self._function(*args, **kwargs)
 
     @abstractmethod
-    def backprop_eqs(self, layer):
+    def backprop_eqs(self, layer, batch_index):
         pass
+
+
+class ReLU(Activation):
+    def __init__(self):
+        super().__init__(lambda x: Max(0, x))
+
+    def backprop_eqs(self, layer, batch_index):
+        dims = layer.result_gradients.dimensions
+
+        return [Eq(layer.result_gradients[dims[0], dims[1], dims[2]],
+                   layer.result_gradients[dims[0], dims[1], dims[2]] *
+                   sign(layer.result[batch_index, dims[0], dims[1], dims[2]]))]
+
+
+class Dummy(Activation):
+    def __init__(self):
+        super().__init__(lambda x: None)
+
+    def backprop_eqs(self, layer, batch_index):
+        return []
