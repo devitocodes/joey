8fdc97b5d3958f8511ee41504e959efd49183486
Maksymilian Graczyk <maksymilian.graczyk18@imperial.ac.uk>
2020-08-13 11:20:50 +0200

    Implement max pooling tests

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -8,7 +8,80 @@ def test_conv():
 
 
 def test_max_pooling():
-    pass
+    layer = joey.MaxPooling(kernel_size=(2, 2), input_size=(2, 2, 3, 3))
+    output = layer.execute(np.array([[[[1, 2, 3],
+                                       [4, 5, 6],
+                                       [7, 8, 9]],
+                                      [[10, 11, 12],
+                                       [13, 14, 15],
+                                       [16, 17, 18]]],
+                                     [[[19, 20, 21],
+                                       [22, 23, 24],
+                                       [25, 26, 27]],
+                                      [[28, 29, 30],
+                                       [31, 32, 33],
+                                       [34, 35, 36]]]]))
+
+    assert(np.array_equal(output, [[[[5, 6],
+                                     [8, 9]],
+                                    [[14, 15],
+                                     [17, 18]]],
+                                   [[[23, 24],
+                                     [26, 27]],
+                                    [[32, 33],
+                                     [35, 36]]]]))
+
+
+def test_max_pooling_larger_stride():
+    layer = joey.MaxPooling(kernel_size=(2, 2), input_size=(2, 2, 3, 3),
+                            stride=(1, 2), strict_stride_check=False)
+    output = layer.execute(np.array([[[[1, 2, 3],
+                                       [4, 5, 6],
+                                       [7, 8, 9]],
+                                      [[10, 11, 12],
+                                       [13, 14, 15],
+                                       [16, 17, 18]]],
+                                     [[[19, 20, 21],
+                                       [22, 23, 24],
+                                       [25, 26, 27]],
+                                      [[28, 29, 30],
+                                       [31, 32, 33],
+                                       [34, 35, 36]]]]))
+
+    assert(np.array_equal(output, [[[[5],
+                                     [8]],
+                                    [[14],
+                                     [17]]],
+                                   [[[23],
+                                     [26]],
+                                    [[32],
+                                     [35]]]]))
+
+
+def test_max_pooling_relu():
+    layer = joey.MaxPooling(kernel_size=(2, 2), input_size=(2, 2, 3, 3),
+                            activation=ReLU())
+    output = layer.execute(np.array([[[[-1, -2, 3],
+                                       [-4, -5, 6],
+                                       [7, 8, 9]],
+                                      [[10, 11, 12],
+                                       [13, -14, -15],
+                                       [16, -17, -18]]],
+                                     [[[19, -20, -21],
+                                       [22, -23, -24],
+                                       [25, 26, 27]],
+                                      [[28, 29, 30],
+                                       [31, 32, 33],
+                                       [34, 35, 36]]]]))
+
+    assert(np.array_equal(output, [[[[0, 6],
+                                     [8, 9]],
+                                    [[13, 12],
+                                     [16, 0]]],
+                                   [[[22, 0],
+                                     [26, 27]],
+                                    [[32, 33],
+                                     [35, 36]]]]))
 
 
 def test_flat():
