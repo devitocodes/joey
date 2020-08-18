17de9d909ee429e4031d35b802f985de74942b5e
Maksymilian Graczyk <maksymilian.graczyk18@imperial.ac.uk>
2020-08-18 21:21:39 +0200

    Decrease tolerance in compare() and add new functions to utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from os import environ
 
 
 def compare(devito, pytorch):
@@ -10,5 +11,19 @@ def compare(devito, pytorch):
     error = abs(devito - pytorch) / abs(pytorch)
     max_error = np.nanmax(error)
 
-    if max_error != np.nan:
-        assert(max_error < 1e-9)
+    if not np.isnan(max_error):
+        assert(max_error < 1e-13)
+
+
+def running_in_parallel():
+    if 'DEVITO_LANGUAGE' not in environ:
+        return False
+
+    return environ['DEVITO_LANGUAGE'] in ['openmp']
+
+
+def get_run_count():
+    if running_in_parallel():
+        return 1000
+    else:
+        return 1
