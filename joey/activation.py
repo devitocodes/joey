d6906711bb4507ebea37573d7f50b2a1edd9612e
Maksymilian Graczyk <maksymilian.graczyk18@imperial.ac.uk>
2020-08-27 21:02:31 +0200

    Add documentation to activation.py

--- joey/activation.py
+++ joey/activation.py
@@ -4,6 +4,19 @@ from devito import Eq
 
 
 class Activation(ABC):
+    """
+    An abstract class representing an activation function.
+
+    When you create a subclass of Activation, you must implement
+    the backprop_eqs() method.
+
+    Parameters
+    ----------
+    function : function
+        A function to apply to data. Usually, it will be a one-argument
+        function f(x) where x is the raw output of a layer.
+    """
+
     def __init__(self, function):
         self._function = function
 
@@ -12,10 +25,20 @@ class Activation(ABC):
 
     @abstractmethod
     def backprop_eqs(self, layer):
+        """
+        Returns a list of Devito equations describing how backpropagation
+        should proceed when encountering this activation function.
+
+        Parameters
+        ----------
+        layer : Layer
+            The next layer in a backpropagation chain.
+        """
         pass
 
 
 class ReLU(Activation):
+    """An Activation subclass corresponding to ReLU."""
     def __init__(self):
         super().__init__(lambda x: Max(0, x))
 
@@ -25,6 +48,7 @@ class ReLU(Activation):
 
 
 class Dummy(Activation):
+    """An Activation subclass corresponding to f(x) = x."""
     def __init__(self):
         super().__init__(lambda x: x)
 
